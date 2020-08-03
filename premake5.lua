b8e48c74fc60239f72076f0e63a688cd7c464b60
flurach <flurach.dev@gmail.com>
2020-08-04 00:26:06 +0300

    Added a boilerplate with premake

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "fpl"
+	configurations { "debug", "release" }
+
+	toolset "clang"
+	cdialect "C89"
+	warnings "Extra"
+
+	BASE_DIR = path.getabsolute(".")
+	targetdir (BASE_DIR .. "/bin")
+	objdir (BASE_DIR .. "/bin/int")
+
+	filter "configurations:debug"
+		symbols "on"
+
+	filter "configurations:release"
+		defines { "NDEBUG" }
+		optimize "on"
+
+include "fpl"
