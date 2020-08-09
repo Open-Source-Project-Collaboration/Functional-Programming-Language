331347dd3e34ebd0edb78700d2707d43ceb5990d
flurach <flurach.dev@gmail.com>
2020-08-09 22:41:48 +0100

    Added some grammar to implement

--- compiler/inc/parsers.h
+++ compiler/inc/parsers.h
@@ -7,9 +7,36 @@
 #include "node.h"
 
 
-/* parsers */
-void parse_statements(lex_t *lex);
-void parse_statement(lex_t *lex);
+/* interface */
+node_t *parse(lex_t *lex);
+
+
+/* <array> ::= "[" <pair> "]" */
+node_t *parse_array(lex_t *lex);
+
+
+/* [INCOMPLETE]
+ * <pair> ::= <pipe> ";" <pair>
+ *          | <pipe> ";" <pipe>
+ */
+node_t *parse_pair(lex_t *lex);
+
+
+/*
+ * <literal> ::= <NAME>
+ *             | <INT>
+ *             | <FLOAT>
+ *             | <STR>
+ *             | <BOOL>
+ *             | <array>
+ */
+node_t *parse_literal(lex_t *lex);
+
+
+/* helpers */
+node_t *parse_many(lex_t *lex, node_t *(*fn)(lex_t *));
+node_t *parse_sequence(lex_t *lex, size_t count, ...);
+node_t *parse_either(lex_t *lex, size_t count, ...);
 
 
 #endif
